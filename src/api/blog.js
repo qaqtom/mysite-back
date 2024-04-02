82ef8f3eea4fc68abe4c72f89b4a3d96acecce49
yiming <526454676@qq.com>
2024-04-02 22:31:44 +0800

    文章管理-文章列表

---
+++ src/api/blog.js
@@ -0,0 +1,22 @@
+import request from '@/utils/request';
+
+//分页获取文章
+//page代表页码，limit代表每一页显示的数量
+export function findBlog(page = 1, limit = 10) {
+    return request({
+        url: '/api/blog',
+        method: 'get',
+        params: {
+            page,
+            limit,
+        }
+    })
+}
+
+//删除文章
+export function delOneBlog(id) {
+    return request({
+        url: `/api/blog/${id}`,
+        method:'delete'
+    })
+}
