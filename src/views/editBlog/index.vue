c043385fdb50661731c5d3d6090df5febdc0f245
yiming <526454676@qq.com>
2024-04-04 11:53:01 +0800

    项目列表，添加项目

--- src/views/editBlog/index.vue
+++ src/views/editBlog/index.vue
@@ -5,12 +5,12 @@
 </template>
 
 <script>
-  import EditArticle from '@/components/EditArticle.vue'
-  export default {
-    components : {
-      EditArticle
-    }
+import EditArticle from '@/components/EditArticle.vue'
+export default {
+  components : {
+    EditArticle
   }
+}
 </script>
 
 <style lang="scss" scoped>
